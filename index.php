e6b13dc3b625f5f265f01657960d1d7670509f59
dharul619 <dharulyuswansyah619@gmail.com>
2023-09-12 07:19:21 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste officiis nisi accusamus asperiores repellendus a perspiciatis reiciendis eius dolor, id dolore nesciunt porro pariatur assumenda nemo harum repudiandae, quae aliquam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
